e01e1adcc098ce65b1d098171761e0f836e54262
Rafael Casas Wechek <rwechek@gmail.com>
2025-08-27 18:04:42 -0500

    Envio al repositorio lo modificado del capitulo 04

--- my-app/ch04/src/app/products/products.module.ts
+++ my-app/ch04/src/app/products/products.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductComponent
   ]
 })
 export class ProductsModule { }
