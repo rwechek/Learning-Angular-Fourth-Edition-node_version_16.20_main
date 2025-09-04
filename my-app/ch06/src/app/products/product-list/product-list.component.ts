e5b1d9bd00611159aeae7aefc0549efafda05711
Rafael Casas Wechek <rwechek@gmail.com>
2025-09-04 17:46:34 -0500

    Envio al repositorio avance para el capitulo 06

--- my-app/ch06/src/app/products/product-list/product-list.component.ts
+++ my-app/ch06/src/app/products/product-list/product-list.component.ts
@@ -1,44 +1,34 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { Product } from '../product';
-
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  providers: [ProductsService]
 })
 export class ProductListComponent implements OnInit, AfterViewInit {
-
   selectedProduct: Product | undefined;
+  products: Product[] = [];
+  @ViewChild(ProductDetailComponent) productDetail:
+    | ProductDetailComponent
+    | undefined;
 
-  products: Product[] = [
-  {
-    name: 'Webcam',
-    price: 100
-},
-  {
-    name: 'Microphone',
-    price: 200
-},
-  {
-    name: 'Wireless keyboard',
-    price: 85
-}
+  constructor(private productService: ProductsService) {}
 
-];
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+  }
 
-  @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
-
-    ngOnInit(): void {}
-      
-    ngAfterViewInit(): void {
-      if (this.productDetail) {
-        console.log(this.productDetail.product);
-      }
+  ngAfterViewInit(): void {
+    if (this.productDetail) {
+      console.log(this.productDetail.product);
     }
+  }
 
-    onBuy(){
-      window.alert(`You just bought ${this.selectedProduct?.name}!`);
-    }
+  onBuy() {
+    window.alert(`You just bought ${this.selectedProduct?.name}!`);
+  }
 }
--- my-app/ch06/src/app/products/products.module.ts
+++ my-app/ch06/src/app/products/products.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { SortPipe } from './sort.pipe';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { ProductViewComponent } from './product-view/product-view.component';
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    SortPipe
+    SortPipe,
+    FavoritesComponent,
+    ProductViewComponent
   ],
   imports: [
     CommonModule
