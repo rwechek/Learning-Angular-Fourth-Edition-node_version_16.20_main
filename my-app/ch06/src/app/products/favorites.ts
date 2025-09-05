4f4cb3df5594868c33bddca704d276e08f17e612
Rafael Casas Wechek <rwechek@gmail.com>
2025-09-05 17:08:22 -0500

    Envio al repositorio el avance del capitulo 06

---
+++ my-app/ch06/src/app/products/favorites.ts
@@ -0,0 +1,12 @@
+import { FavoritesService } from './favorites.service';
+import { ProductsService } from './products.service';
+import { ProductViewService } from './product-view/product-view.service';
+
+export function favoritesFactory(isFavorite: boolean) {
+  return (productViewService: ProductViewService) => {
+    if (isFavorite) {
+      return new FavoritesService();
+    }
+    return new ProductsService();
+  };
+}
--- my-app/ch06/src/app/products/favorites/favorites.component.ts
+++ my-app/ch06/src/app/products/favorites/favorites.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
+import { FavoritesService } from '../favorites.service';
+import { favoritesFactory } from '../favorites';
+import { ProductViewService } from '../product-view/product-view.service';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.css']
+  styleUrls: ['./favorites.component.css'],
+  //providers: [{ provide: ProductsService, useClass: FavoritesService }]
+  providers: [{ provide: ProductsService, useFactory: favoritesFactory(true), deps: [ProductViewService] }],
 })
 export class FavoritesComponent implements OnInit {
   products: Product[] = [];
@@ -15,5 +20,4 @@ export class FavoritesComponent implements OnInit {
   ngOnInit(): void {
     this.products = this.productService.getProducts();
   }
-
 }
